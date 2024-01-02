69d7aa7c34dabbbcb318d00de56511a503a4d94b
mxlle <almut.m.f@gmail.com>
2024-01-02 16:08:26 +0100

    fix: layout improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,8 @@ import { RouterOutlet } from '@angular/router';
         max-width: 1280px;
         margin: 0 auto;
         text-align: center;
-        padding: 16px;
+        height: 100%;
+        width: 100%;
       }
     `,
   ],
