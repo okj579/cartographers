7461b7494f1d62517d3c6c958253d76acc20d8af
mxlle <almut.m.f@gmail.com>
2024-02-11 20:48:17 +0100

    fix: board tile component import

--- src/app/components/game-board/board-tile.component.ts
+++ src/app/components/game-board/board-tile.component.ts
@@ -2,22 +2,12 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { BoardTile } from '../../../models/board-tile';
 import { NgForOf, NgIf } from '@angular/common';
 import { MONSTER_MAP } from '../../../models/monster';
-import { SeasonBadgeComponent } from './season-badge/season-badge.component';
 
 @Component({
   selector: 'app-board-tile',
   standalone: true,
-  imports: [NgIf, SeasonBadgeComponent, NgForOf],
-  template: ` <div *ngIf="monsterEmoji" class="monster-emoji">{{ monsterEmoji }}</div>
-    <div *ngIf="tile.destroyed" class="destroyed"></div>
-    <div *ngIf="tile.monsterType" class="monster-type">{{ tile.monsterType }}</div>
-    <div
-      *ngIf="tile.hasCoin || tile.wasScoreCoin"
-      class="coin"
-      [class.coin-was-removed]="tile.wasScoreCoin"
-      [class.will-be-removed]="coinWillBeRemoved"
-      [class.is-dragon]="tile.monsterType"
-    ></div>`,
+  imports: [NgIf, NgForOf],
+  templateUrl: './board-tile.component.html',
   styleUrl: './board-tile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
